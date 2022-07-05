d3a9fe0b1bb46ee9446066287b7e5220f84fc41d
comemaino <mat.maino@gmail.com>
2022-07-05 14:09:26 +0200

    Add: guest.home, App.vue, WorkInProgress.vue

---
+++ resources/js/components/WorkInProgress.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    This page is under maintenance
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'WorkInProgress'
+}
+</script>
+
+<style>
+</style> 
--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-
-
 Auth::routes();
 
 Route::middleware('auth')
