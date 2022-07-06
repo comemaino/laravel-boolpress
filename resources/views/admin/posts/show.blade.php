16255ecda79aea8743c04ed0807eaac8b9f44937
comemaino <mat.maino@gmail.com>
2022-07-06 14:09:38 +0200

    Add: destroy function

--- resources/views/admin/posts/show.blade.php
+++ resources/views/admin/posts/show.blade.php
@@ -6,4 +6,10 @@
 	<p>{{ $post->content }}</p>
 
 	<a href="{{ route('admin.posts.edit', ['post' => $post->id]) }}">Edit</a>
+	<form action="{{ route('admin.posts.destroy', ['post' => $post->id]) }}" method="POST">
+		@csrf
+		@method('DELETE')
+		<button type="submit" class="btn btn-danger mt-3"
+			onclick="confirm('Do you really want to delete this post?')">Delete</button>
+	</form>
 @endsection
